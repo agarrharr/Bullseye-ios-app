9b2a8ba660a557ac79c6eaa90cc416f62ff76e83
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-04-01 11:29:02 -0600

    Start making buttons

---
+++ Bullseye/Views/RoundedViews.swift
@@ -0,0 +1,47 @@
+//
+//  RoundViews.swift
+//  Bullseye
+//
+//  Created by Adam Garrett-Harris on 4/1/21.
+//  Copyright © 2021 Adam Garrett-Harris. All rights reserved.
+//
+
+import SwiftUI
+
+struct RoundedImageViewStroked: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56.0, height: 56.0)
+    }
+}
+
+struct RoundedImageViewFilled: View {
+    var systemName: String
+    
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56.0, height: 56.0)
+    }
+}
+
+struct PreviewView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
+    }
+}
+
+struct RoundViews_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewView()
+        PreviewView().preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
